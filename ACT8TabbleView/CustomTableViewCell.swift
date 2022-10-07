d2d0fd395936d4856065ecf209bd1834a9c9075c
Jair <jairntonio21@hotmail.com>
2022-10-07 11:22:50 -0500

    1.2

--- ACT8TabbleView/CustomTableViewCell.swift
+++ ACT8TabbleView/CustomTableViewCell.swift
@@ -13,4 +13,6 @@ class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var iconImageView: UIImageView!
     
     @IBOutlet weak var label: UILabel!
+    
+    
 }
---
+++ ACT8TabbleView/MazdaTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MazdaTableViewCell.swift
+//  ACT8TabbleView
+//
+//  Created by Alumno on 07/10/22.
+//
+
+import UIKit
+
+class MazdaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+    @IBOutlet weak var titulo: UILabel!
+    
+    @IBOutlet weak var reseña: UILabel!
+}
